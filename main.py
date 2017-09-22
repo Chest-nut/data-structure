7f3d891d5c53822ff4b472c5da3f8ac1fd8185ab
jikexuyuan <383105690@qq.com>
2017-09-22 11:44:44 +0800

    add test_sort

--- main.py
+++ main.py
@@ -4,33 +4,17 @@ import time
 import random
 from copy import copy, deepcopy
 
-from sortalgorithm import Sort
+from sortalgorithm import SortHelper
 
 
 if __name__ == '__main__':
-    sort_ = Sort()
+    sort_ = SortHelper()
 
-    # # 比较两种插入算法的效率：
-    #
-    # array = random.sample(range(5000), 5000)  # 生成一组包含5000个乱序数字的列表
-    # # array = random.sample(range(5), 5)
-    # array_copy = copy(array)    # 拷贝一份一样的乱序数组
-    #
-    # time1 = time.time()
-    # sort_.insertion_sort1(array)
-    # # sort_.view()
-    # time2 = time.time()
-    # sort_.insertion_sort2(array_copy)
-    # # sort_.view()
-    # time3 = time.time()
-    # print(time2 - time1)    # 1.077385425567627s
-    # print(time3 - time2)    # 3.2283499240875244s
+    array = random.sample(range(2000), 2000)  # 生成一组包含5000个乱序数字的列表
+    array_copy = copy(array)    # 拷贝一份一样的乱序数组
+    array_copy2 = copy(array)
 
-    # 选择排序算法部分：
-    array = random.sample(range(5000), 5000)
-    # print(array)
-    time1 = time.time()
-    arr = sort_.selection_sort(array)
-    time2 = time.time()
-    # print(arr)
-    print(time2 - time1)
+    sort_.test_sort(array, sort_.insertion_sort1, '插入排序1')
+    sort_.test_sort(array_copy, sort_.insertion_sort2, '插入排序2')
+    sort_.test_sort(array_copy2, sort_.selection_sort, '选择排序')
+
